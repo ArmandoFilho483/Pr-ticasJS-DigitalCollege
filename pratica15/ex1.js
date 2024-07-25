823be2455821b33cae8a8308c03d6fb91c609817
ArmandoFilho483 <armandopdff@gmail.com>
2024-07-25 09:43:22 -0300

    Adição de todos as práticas realizadas até esta data(25/07/24).

---
+++ pratica15/ex1.js
@@ -0,0 +1,6 @@
+const estudante = ['João', 'Paulo', 'Mateus', 'Pedro', 'Tiago'];
+const nota = [8, 5, 2, 10, 9];
+
+for (let i = 0; i < estudante.length; i++) {
+    console.log(`O estudante ${estudante[i]} tirou a nota ${nota[i]}`);
+}
---
+++ pratica15/ex2.js
@@ -0,0 +1,5 @@
+const valor = null;
+
+while () {
+    
+}
